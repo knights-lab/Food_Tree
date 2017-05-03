e2c4e2b1a290a858be713da133479caf06acff5d
pvangay <zaubme@gmail.com>
2017-05-03 09:59:16 -0500

    check.db update
    now outputs any missing unique main food descriptions so that foods
    with and without FoodIDs can be identified

--- R/lib/check.db.r
+++ R/lib/check.db.r
@@ -4,7 +4,7 @@ check.db <- function(food_database_fn, food_records_fn, output_fn)
     fdata <- read.table(food_database_fn, header = TRUE, sep="\t", colClasses="character", quote="", strip.white=T)
     diet <- read.table(food_records_fn, header = TRUE, sep="\t", colClasses="character", quote="", strip.white=T)
     
-    foods.missing <- unique(diet[!(diet$FoodID %in% fdata$FoodID), ])
+    foods.missing <- unique(diet[!(diet$FoodID %in% fdata$FoodID), c("Main.food.description", "FoodID")])
     
     write.table(foods.missing, output_fn, sep = "\t", quote = FALSE, row.names = FALSE)
     
