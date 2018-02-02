7418fa2d81154fb0367931f89274aecc35555617
abbycole <abigail.j.cole@gmail.com>
2018-02-02 13:00:17 -0600

    Some changes
    Making NHANES food tree

--- R/bin/make.ibs.tree.r
+++ R/bin/make.ibs.tree.r
@@ -6,6 +6,9 @@ source("lib/check.db.r")
 source("lib/format.foods.r")
 source("lib/make.food.tree.r")
 source("lib/make.food.otu.r")
+source("lib/make.dhydrt.otu.r")
+source("lib/make.fiber.otu.R")
+source("lib/filter.db.by.diet.records.r")
 
 # note: ASA24 now has modcodes
 #format.foods(input_fn="../raw data/all.food.desc.txt", output_fn="data/MCT/ASA24Database.txt")
@@ -22,8 +25,34 @@ make.food.tree(nodes_fn="data/NodeLabels.txt",
                food_database_fn="data/MCT/ASA24Database.txt", 
                output_tree_fn="output/ibs.tree.txt", 
                output_taxonomy_fn = "output/ibs.taxonomy.txt")
-    
+
+# create a reduced node tree that will show just the foods consumed in the study (for visualizations)
+### redo tree/taxonomy/otu generation with only the foods actually reported
+filter.db.by.diet.records(food_database_fn="data/MCT/ASA24Database.txt",
+                          food_records_fn="data/IBS/dietrecords.txt", 
+                          output_fn="output/ibs.foodreportedonly.txt")
+
+make.food.tree(nodes_fn="data/NodeLabels.txt", 
+               food_database_fn = "output/ibs.foodreportedonly.txt", 
+               addl_foods_fn= "data/IBS/missing.txt", 
+               output_tree_fn="output/ibs.tree.foodsreportedonly.txt", 
+               output_taxonomy_fn = "output/ibs.taxonomy.foodsreportedonly.txt")
+
+
+
+
+# make the different food-out tables for downstream analysis
 make.food.otu(food_records_fn="data/IBS/dietrecords.txt", 
               food_record_id = "Timepoint.ID", 
               food_taxonomy_fn="output/ibs.taxonomy.txt", 
               output_fn = "output/ibs.food.otu.txt")
+
+make.dhydrt.otu(food_records_fn="data/IBS/dietrecords.txt", 
+                food_record_id = "Timepoint.ID", 
+                food_taxonomy_fn="output/ibs.taxonomy.txt", 
+                output_fn = "output/ibs.dhydrt.otu.txt")
+
+make.fiber.otu(food_records_fn="data/IBS/dietrecords.txt", 
+               food_record_id = "Timepoint.ID", 
+               food_taxonomy_fn="output/ibs.taxonomy.txt", 
+               output_fn = "output/ibs.fiber.otu.txt")
