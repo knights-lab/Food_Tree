7ad96b3f7c33749eec9876687c116d28df3e6b0f
abbycole <abigail.j.cole@gmail.com>
2018-04-12 09:51:26 -0500

    Added comments

--- R/bin/make.rygb.tree.r
+++ R/bin/make.rygb.tree.r
@@ -11,9 +11,9 @@ source("lib/make.dhydrt.otu.r")
 source("lib/filter.db.by.diet.records.r")
 
 # Will need: ASA24 database - previously made: available at "data/RYGB/ASA24Database.txt"
-format.foods(input_fn="data/NHANES/processed/foodcodes.txt", output_fn="data/NHANES/NHANESDatabase.txt") # build database from main codes
+# format.foods(input_fn="data/NHANES/processed/foodcodes.txt", output_fn="data/NHANES/NHANESDatabase.txt") # build database from main codes
 # format the items file that contains all of the foods - previously pre-processed this file to make txt and remove ",',&,%
-# Also need to change the name of the Food_Description field to "Main.food.description
+# Also need to change the name of the Food_Description field to "Main.food.description"
 format.foods(input_fn="data/RYGB/raw/items_file.txt", output_fn="data/RYGB/dietrecords.txt", dedupe=F)
 
 # next check to make sure the database has all the foods in the foods file
@@ -21,8 +21,8 @@ check.db(food_database_fn = "data/RYGB/ASA24Database.txt", food_records_fn="data
 
 # make the food tree file
 # in this case there are no missing items, so no need to add them here
-make.food.tree(nodes_fn="data/NodeLabels.txt", 
-               food_database_fn="data/RYGB/ASA24Database.txt",
+make.food.tree(nodes_fn="data/NodeLabels.txt", #the backbone to this whole thing
+               food_database_fn="data/RYGB/ASA24Database.txt", #previously formatted above
                output_tree_fn="output/rygb.tree.txt", 
                output_taxonomy_fn = "output/rygb.taxonomy.txt")
 
