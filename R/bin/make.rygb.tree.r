206b0d2f8430ab0e8ddeef92f10f0878440fc7bc
abbycole <abigail.j.cole@gmail.com>
2018-07-17 15:57:38 -0500

    Update to RYGB usage pipeline
    Includes updates to raw data and output files.

--- R/bin/make.rygb.tree.r
+++ R/bin/make.rygb.tree.r
@@ -14,20 +14,20 @@ source("lib/filter.db.by.diet.records.r")
 # format.foods(input_fn="data/NHANES/processed/foodcodes.txt", output_fn="data/NHANES/NHANESDatabase.txt") # build database from main codes
 # format the items file that contains all of the foods - previously pre-processed this file to make txt and remove ",',&,%
 # Also need to change the name of the Food_Description field to "Main.food.description"
-format.foods(input_fn="data/RYGB/raw/items_file.txt", output_fn="data/RYGB/dietrecords.txt", dedupe=F)
+format.foods(input_fn="data/RYGB/raw/RYGBLT_82285_Items.txt", output_fn="data/RYGB/dietrecords.txt", dedupe=F)
 
 # next check to make sure the database has all the foods in the foods file
 check.db(food_database_fn = "data/RYGB/ASA24Database.txt", food_records_fn="data/RYGB/dietrecords.txt", output_fn="data/RYGB/missing.txt")
 
 # make the food tree file
 # in this case there are no missing items, so no need to add them here
-make.food.tree(nodes_fn="data/NodeLabels.txt", #the backbone to this whole thing
+make.food.tree(nodes_fn="data/NodeLabelsMCT.txt", #the backbone to this whole thing
                food_database_fn="data/RYGB/ASA24Database.txt", #previously formatted above
                output_tree_fn="output/rygb.tree.txt", 
                output_taxonomy_fn = "output/rygb.taxonomy.txt")
 
 # this makes the food otu table as dehydrated grams per kcal
-make.dhydrt.otu(food_records_fn="data/RYGB/dietrecords.txt", food_record_id = "UserName", food_taxonomy_fn="output/rygb.taxonomy.txt", 
+make.dhydrt.otu(food_records_fn="data/RYGB/dietrecords.txt", food_record_id = "SampleID", food_taxonomy_fn="output/rygb.taxonomy.txt", 
                 output_fn = "output/rygb.dhydrt.txt")
 
 
@@ -36,7 +36,7 @@ filter.db.by.diet.records(food_database_fn="data/RYGB/ASA24Database.txt",
                           food_records_fn="data/RYGB/dietrecords.txt", 
                           output_fn="output/rygbfoodreportedonly.txt")
 
-make.food.tree(nodes_fn="data/NodeLabels.txt", 
+make.food.tree(nodes_fn="data/NodeLabelsMCT.txt", 
                food_database_fn = "output/rygbfoodreportedonly.txt", 
                output_tree_fn="output/rygb.tree.foodsreportedonly.txt", 
                output_taxonomy_fn = "output/rygb.taxonomy.foodsreportedonly.txt")
